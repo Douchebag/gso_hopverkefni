d30e9bc0c9d013e784838091295acfe89c2737e2
sigurdurBl <sigurdureg210@nemi.tskoli.is>
2017-04-26 17:21:17 +0000

    bæti við fæslum

--- diskur.sql
+++ diskur.sql
@@ -2,9 +2,9 @@ INSERT INTO diskur
 (nafn, utgafudagur, fjoldi_laga, lengd_sek)
 VALUES
 ("Damn", "2017-04-14", "14", "3246"),
-("", "", "", ""),
-("", "", "", ""),
-("", "", "", ""),
+("by myself", "2017-04-21", "11", "2700"),
+("Americana", "2017-04-21", "15", "3480"),
+("Love and War", "Memories: Do Not Open", "12", "2580"),
 ("", "", "", ""),
 ("", "", "", ""),
 ("", "", "", ""),
