149ed958118c9ac42c3abd5e63097efd2d8366c7
Ððµ¢hêßåg <yesimdingo@gmail.com>
2017-04-26 16:56:43 +0000

    bjó til töflu til að byrja á

---
+++ flyjandi.sql
@@ -0,0 +1,4 @@
+INSERT INTO flytjandi
+(nafn, faedingardagur, Danardagur)
+VALUES
+("nafn", "1957-03-02", "1932-02-22")
---
+++ utgefandi.sql
@@ -0,0 +1,5 @@
+INSERT INTO utgefandi
+(nafn, stofnandi, ar_stofnad)
+VALUES
+("Aftermath Entertainment", "Dr. Dre", "1996-04-25"),
+("", "", ""),
