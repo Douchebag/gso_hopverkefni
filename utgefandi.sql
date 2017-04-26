bba4116f4ea5183f74b3f9fb4d4c7af6e5b176f5
sigurdurBl <sigurdureg210@nemi.tskoli.is>
2017-04-26 17:12:19 +0000

    bæti við 2 fæslum

--- utgefandi.sql
+++ utgefandi.sql
@@ -2,4 +2,5 @@ INSERT INTO utgefandi
 (nafn, stofnandi, ar_stofnad)
 VALUES
 ("Aftermath Entertainment", "Dr. Dre", "1996-04-25"),
-("", "", ""),
+("Sony Music", "arc", "1929-03-23"),
+(,"Warner Bros","Harry Warner","1923-04-04")
