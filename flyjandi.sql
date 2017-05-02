cdd1c3e21d1d2c7d2c632b156bc3e0d3807dbf55
Douchebag <yesimdingo@gmail.com>
2017-05-02 14:35:06 +0000

    Lag, flytjandi og diskur
    Bjo til tofluna lag og baetti vid faerslum i flytjandi og diskur

--- flyjandi.sql
+++ flyjandi.sql
@@ -1,4 +1,9 @@
 INSERT INTO flytjandi
 (nafn, faedingardagur, Danardagur)
 VALUES
-("nafn", "1957-03-02", "1932-02-22")
+("Gorillaz", "1998-03-02", NULL),
+("Kendrick Lamar", "1987-06-17", NULL),
+("Ray Davies", "1944-06-21", NULL),
+("Sheryl Crow", "1962-02-11", NULL),
+("The Chainsmokers", "2012-11-29", NULL),
+("Tupac Shakur", "1971-06-16", "1996-09-13")
---
+++ lag.sql
@@ -0,0 +1,415 @@
+INSERT INTO lag
+(nafn, lengd_sek, texti)
+VALUES
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("", "", ""),
+("Carnival", "135", "[Intro]
+Carnival
+
+[Verse 1: Anthony Hamilton]
+We play to win, just to find the dark
+Go home again, and we both fall out
+I wanna spend this time with you
+Doing nothing
+Keep you satisfied, while they rob us blind
+I hear the music in the background
+Singing, singing
+
+[Bridge: Anthony Hamilton]
+Oh, I haven't see the glory
+Coming, coming, coming
+And we play
+We play, we play, we play like a-
+
+[Chorus: Anthony Hamilton & Choir]
+Just like a carnival
+Game, just like a carnival
+Laugh, just like a carnival
+Joke, just like a-
+Spinning me round and around and around
+Just like a carnival
+
+[Verse 2: Anthony Hamilton]
+We play to win, just to find the dark
+Go again, and we both fall out
+I wanna spend the time with you
+Doing nothing
+
+[Chorus: Anthony Hamilton & Choir]
+We, just like a carnival
+We laughed, just like a carnival
+Sleep with the dead, just like a carnival
+Playing with your mind, jJust like a-
+Spinning me round and around and around
+Just like a carnival, yeah"),
+("Ascension", "155", "[Intro: Vince Staples]
+You are now tuned into the tomb of Jehova
+Play my tunes loud enough to shake the room, what's the hold up?
+Heard the world is ending soon I assumed that they told ya
+They tryna dinosaur us
+So now it's time to go up
+
+[Chorus: Vince Staples & The Humanz Choir]
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+
+[Verse 1: Vince Staples]
+The roof is on fire
+She wet like Barbra Streisand
+Police everywhere
+It's like a nigga killed a white man
+I just wanna find a baby mama 'fore the night end
+So don't be coming 'round Vince
+On that batter ram shit
+On that 911 shit
+Need a misses brown as Missy
+I can have some fun with
+Them slave bones make Grace Jones
+Don't stop, bitch, stay focused
+They hated on us since days of Moses
+Let my people go crazy
+Them stars falling, don't chase 'em
+
+[Chorus: Vince Staples & The Humanz]
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+
+[Verse 2: 2D]
+Attack on Iraq
+It's on a line
+Typing it fast
+Then it's slipping my mind
+Oh, when I'm drunk
+I'm spirited back
+I fall in a fire then I save myself
+Then I get advantage
+'Cause the feeling's so raw
+In these times of sedition
+Well, nothing is dull
+And I'm moving into stillness
+And recalling it all
+And I’m a lover, just a lover, just a lover, just a lover, just a-
+
+[Chorus: Vince Staples & The Humanz]
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+
+[Verse 3: Vince Staples]
+All these liberated women sitting in my lap
+I'm finna catch a body like I got a gun and badge
+I'm finna turn Obama to my patna 'fore he dash
+Pull up to the pad, wipe my ass with the flag
+I'm just playing, baby, this the land of the free
+Where you can get a Glock and a gram for the cheap
+Where you can live your dreams long as you don't look like me
+Be a puppet on a string, hanging from a fucking tree
+(Aghh!)
+
+[Chorus: Vince Staples & The Humanz]
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)
+The sky's falling, baby
+Drop that ass 'fore it crash (higher)"),
+("YAH.", "160", "[Intro: Kid Capri]
+New shit, new Kung Fu Kenny
+
+[Verse 1]
+I got so many theories and suspicions
+I'm diagnosed with real nigga conditions
+Today is the day I follow my intuition
+Keep the family close—get money, fuck bitches
+I double parked the Aston in the red
+My mama told me that I'ma work myself to death
+My girl told me don't let these hoes get in my head
+My world been ecstatic, I checked the signal that read—
+
+[Chorus]
+Buzzin', radars is buzzin'
+Yah, yah, yah, yah
+Yah, yah, yah, yah, yah, yah
+Buzzin', radars is buzzin'
+Yah, yah, yah, yah
+Yah, yah, yah, yah, yah, yah
+
+[Verse 2]
+Interviews wanna know my thoughts and opinions
+Fox News wanna use my name for percentage
+My latest muse is my niece, she worth livin'
+See me on the TV and scream: "That's Uncle Kendrick!"
+Yeah, that's the business
+Somebody tell Geraldo this nigga got some ambition
+I'm not a politician, I'm not 'bout a religion
+I'm a Israelite, don't call me Black no mo'
+That word is only a color, it ain't facts no mo'
+My cousin called, my cousin Carl Duckworth
+Said know my worth
+And Deuteronomy say that we all been cursed
+I know he walks the Earth
+But it's money to get, bitches to hit, yah
+Zeroes to flip, temptation is, yah
+First on my list, I can't resist, yah
+Everyone together now, know that we forever—
+
+[Chorus]
+Buzzin', radars is buzzin'
+Yah, yah, yah, yah
+Yah, yah, yah, yah, yah, yah
+Buzzin', radars is buzzin'
+Yah, yah, yah, yah
+Yah, yah, yah, yah, yah, yah"),
+("LOVE.", "211", "[Intro: Kendrick Lamar]
+Damn, love or lust
+Damn, all of us
+
+[Chorus: Zacari & Kendrick Lamar]
+Give me a run for my money
+There is nobody, no one to outrun me
+(Another world premiere!)
+So give me a run for my money
+Sippin' bubbly, feelin’ lovely, livin' lovely
+Just love me
+I wanna be with you, ayy, I wanna be with
+Just love me, just love me, just love me
+I wanna be with you, ayy, I wanna be with
+Love me
+I wanna be with you
+Love me, just love me
+
+[Refrain: Kendrick Lamar & Zacari]
+If I didn't ride blade on curb, would you still love me?
+If I minimized my net worth, would you still love me?
+Keep it a hundred, I’d rather you trust me than to love me
+Keep it a whole one hund': don't got you, I got nothin'
+
+[Verse 1: Kendrick Lamar]
+Ayy, I got somethin'
+Hol' up, we gon' function, no assumptions
+Feelin' like Tyson with it
+Knock it out twice, I’m with it
+Only for the night, I’m kiddin'
+Only for life, you’re a homie for life
+You're a homie for life, let's get it
+Hit that shoulder lean
+I know what comin' over mean
+Backstroke oversea
+I know what you need
+Already on ten, our money come in
+All feeling go out, this feeling don’t drought
+This party won't end
+
+[Refrain: Kendrick Lamar & Zacari]
+If I didn't ride blade on curb, would you still love me?
+If I minimized my net worth, would you still love me?
+Keep it a hundred, I'd rather you trust me than to love me
+Keep it a whole one hund': don't got you, I got nothin'
+
+[Chorus: Zacari & Kendrick Lamar]
+Give me a run for my money
+There is nobody, no one to outrun me
+So give me a run for my money
+Sippin' bubbly, feelin' lovely, livin' lovely
+Just love me
+I wanna be with you, ayy, I wanna be with
+Just love me, just love me, just love me
+I wanna be with you, ayy, I wanna be with
+Love me
+I wanna be with you
+Love me, just love me
+
+[Verse 2: Kendrick Lamar]
+I'm on the way
+We ain't got no time to waste
+Poppin' your gum on the way
+Am I in the way?
+I don't wan' pressure you none
+I want your blessing today
+Oh, by the way, open the door by the way
+Told you that I'm on the way
+I'm on the way, I know connection is vague
+Pick up the phone for me, babe
+Damn it, we jammin'
+Bad attitude from yo' nanny
+Curves and your hips from yo' mammy
+Remember Gardena, I took the studio camera
+I know Top will be mad at me
+I had to do it, I want your body, your music
+I bought the big one to prove it
+Look what you made
+Told you that I'm on the way
+I'm like an exit away, yep
+
+[Refrain: Kendrick Lamar & Zacari]
+If I didn't ride blade on curb, would you still love me?
+If I minimized my net worth, would you still love me?
+Keep it a hundred, I'd rather you trust me than to love me
+Keep it a whole one hund': don't got you, I got nothin'
+
+[Chorus: Zacari & Kendrick Lamar]
+Give me a run for my money
+There is nobody, no one to outrun me
+So give me a run for my money
+Sippin' bubbly, feelin' lovely, livin' lovely
+Just love me
+I wanna be with you, ayy, I wanna be with
+Just love me, just love me, just love me
+I wanna be with you, ayy, I wanna be with
+Love me
+I wanna be with you
+Love me, just love me"),
+("DNA.", "185", "[Verse 1]
+I got, I got, I got, I got
+Loyalty, got royalty inside my DNA
+Cocaine quarter piece, got war and peace inside my DNA
+I got power, poison, pain and joy inside my DNA
+I got hustle though, ambition, flow, inside my DNA
+I was born like this, since one like this
+Immaculate conception
+I transform like this, perform like this
+Was Yeshua's new weapon
+I don’t contemplate, I meditate, then off your fucking head
+This that put-the-kids-to-bed
+This that I got, I got, I got, I got
+Realness, I just kill shit 'cause it's in my DNA
+I got millions, I got riches buildin’ in my DNA
+I got dark, I got evil, that rot inside my DNA
+I got off, I got troublesome heart inside my DNA
+I just win again, then win again like Wimbledon, I serve
+Yeah, that's him again, the sound that engine in is like a bird
+You see fireworks and Corvette tire skrrt the boulevard
+I know how you work, I know just who you are
+See, you's a, you's a, you's a—
+Bitch, your hormones prolly switch inside your DNA
+Problem is, all that sucker shit inside your DNA
+Daddy prolly snitched, heritage inside your DNA
+Backbone don't exist, born outside a jellyfish, I gauge
+See, my pedigree most definitely don't tolerate the front
+Shit I've been through prolly offend you
+This is Paula’s oldest son
+I know murder, conviction
+Burners, boosters, burglars, ballers, dead, redemption
+Scholars, fathers dead with kids
+And I wish I was fed forgiveness
+Yeah, yeah, yeah, yeah, soldier’s DNA
+Born inside the beast
+My expertise checked out in second grade
+When I was 9, on-sale motel, we didn't have nowhere to stay
+At 29, I’ve done so well, hit cartwheel in my estate
+And I'm gon' shine like I'm supposed to
+Antisocial, extrovert
+And excellent mean the extra work
+And absentness what the fuck you heard
+And pessimists never struck my nerve
+And Nazareth gonna plead his case
+The reason my power’s here on earth
+Salute the truth, when the prophet say
+
+[Bridge]
+I got loyalty, got royalty inside my DNA
+This is why I say that hip hop has done more damage to young African Americans than racism in recent years
+I got loyalty, got royalty inside my DNA
+I live a better life, I'm rollin' several dice, fuck your life
+I got loyalty, got royalty inside my DNA
+I live a better, fuck your life
+5, 4, 3, 2, 1
+This is my heritage, all I'm inheritin'
+Money and power, the mecca of marriages
+
+[Verse 2]
+Tell me somethin'
+You mothafuckas can't tell me nothin'
+I'd rather die than to listen to you
+My DNA not for imitation
+Your DNA an abomination
+This how it is when you're in the Matrix
+Dodgin' bullets, reapin' what you sow
+And stackin' up the footage, livin' on the go
+And sleepin' in a villa
+Sippin' from a Grammy and walkin' in the buildin'
+Diamond in the ceilin', marble on the floors
+Beach inside the window, peekin' out the window
+Baby in the pool, godfather goals
+Only Lord knows, I've been goin' hammer
+Dodgin' paparazzi, freakin' through the cameras
+Eat at Four Daughters, Brock wearin' sandals
+Yoga on a Monday, stretchin' to Nirvana
+Watchin' all the snakes, curvin' all the fakes
+Phone never on, I don't conversate
+I don't compromise, I just penetrate
+Sex, money, murder—these are the breaks
+These are the times, level number 9
+Look up in the sky, 10 is on the way
+Sentence on the way, killings on the way
+Motherfucker, I got winners on the way
+You ain't shit without a body on your belt
+You ain't shit without a ticket on your plate
+You ain't sick enough to pull it on yourself
+You ain't rich enough to hit the lot and skate
+Tell me when destruction gonna be my fate
+Gonna be your fate, gonna be our faith
+Peace to the world, let it rotate
+Sex, money, murder—our DNA
+
+[Video Outro]
+DNA
+Gimme some ganja, gimme some ganja
+DNA
+Gimme some ganja
+Real nigga in my DNA
+Ain't no ho inside my DNA
+Drippin' gold inside my DNA
+Power shows inside my DNA
+DNA
+Gimme some ganja, gimme some ganja
+Real nigga in my DNA
+Ain't no ho inside my DNA"),
+("BLOOD.", "118", "[Intro: Bēkon]
+Is it wickedness?
+Is it weakness?
+You decide
+Are we gonna live or die?
+
+[Verse: Kendrick Lamar]
+So I was takin' a walk the other day, and I seen a woman—a blind woman—pacin' up and down the sidewalk. She seemed to be a bit frustrated, as if she had dropped somethin' and havin' a hard time findin' it. So after watchin' her struggle for a while, I decide to go over and lend a helping hand, you know? "Hello, ma'am, can I be of any assistance? It seems to me that you have lost something. I would like to help you find it." She replied: "Oh yes, you have lost something. You've lost... your life."
+
+{Gunshot}
+
+[Bridge: Bēkon]
+Is it wickedness?
+
+[Outro]
+Lamar stated his views on police brutality with that line in the song, quote: "And we hate the popo, wanna kill us in the street fo' sho'…"
+Oh please, ugh, I don't like it.")
