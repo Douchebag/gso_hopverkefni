c09f005193495320946a4ee4473e06035cda7ffd
Ððµ¢hêßåg <yesimdingo@gmail.com>
2017-05-03 17:33:22 +0000

    s

--- flyjandi.sql
+++ flyjandi.sql
@@ -1,9 +1,9 @@
 INSERT INTO flytjandi
-(nafn, faedingardagur, Danardagur)
+(nafn, faedingardagur, Danardagur, thema_id, flokkur_ID, lag_ID)
 VALUES
-("Gorillaz", "1998-03-02", NULL),
-("Kendrick Lamar", "1987-06-17", NULL),
-("Ray Davies", "1944-06-21", NULL),
-("Sheryl Crow", "1962-02-11", NULL),
-("The Chainsmokers", "2012-11-29", NULL),
-("Tupac Shakur", "1971-06-16", "1996-09-13")
+("Gorillaz", "1998-03-02", NULL, "15, 16, 18, 19, ", "6", "18, 19"),
+("Kendrick Lamar", "1987-06-17", NULL, "1, 2, 3, 4, 5, 6, 7", "3", "3, 8, 9, 12, 13, 20"),
+("Ray Davies", "1944-06-21", NULL, "", "", "1, 2"),
+("Sheryl Crow", "1962-02-11", NULL, "", "", "21"),
+("The Chainsmokers", "2012-11-29", NULL, "", "", "22"),
+("Tupac Shakur", "1971-06-16", "1996-09-13", "", "", "4, 5, 6, 7, 11, 14, 15, 16, 17")
