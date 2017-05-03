1925797a4066bbf47c29ab79808b33a1e3c4f00d
Ððµ¢hêßåg <yesimdingo@gmail.com>
2017-05-03 16:38:29 +0000

    lagað

--- utgefandi.sql
+++ utgefandi.sql
@@ -3,4 +3,5 @@ INSERT INTO utgefandi
 VALUES
 ("Aftermath Entertainment", "Dr. Dre", "1996-04-25"),
 ("Sony Music", "arc", "1929-03-23"),
-(,"Warner Bros","Harry Warner","1923-04-04")
+("Warner Bros", "Harry Warner", "1923-04-04"),
+("Death Row Records", "Dr. Dre, Suge Knight", "1991-11-23")
