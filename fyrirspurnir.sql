f2eb1dd075fd30eeacf4e4dd83982cc9879b5ec0
Douchebag <yesimdingo@gmail.com>
2017-05-10 12:16:45 +0000

    3

--- fyrirspurnir.sql
+++ fyrirspurnir.sql
@@ -3,20 +3,21 @@ SELECT lag.nafn
 FROM diskur
 INNER JOIN lag
 ON lag.diskur_ID = diskur.ID
+WHERE diskur.nafn like 'Damn'
 
 b.
 SELECT lag.nafn
-FROM flytjandi
-INNER JOIN lag
-ON *******
-WHERE nafn like 'Kendrick Lamar'
+FROM lag
+INNER JOIN flytjandi
+ON flytjandi.lag_ID = lag.ID
+WHERE flytjandi.nafn like 'Kendrick Lamar'
 
 c.
 SELECT lag.nafn
 FROM tegund
 INNER JOIN lag
 ON lag.tegund_ID = tegund.ID
-WHERE nafn like 'Rap'
+WHERE tegund.nafn like 'Rap'
 
 d.
 SELECT nafn
@@ -28,15 +29,24 @@ SELECT nafn
 FROM diskur
 WHERE utgafudagur > '2010-01-01'
 
-f.
+f.*****
 SELECT diskur.nafn, flytjandi.nafn, diskur.utgafudagur
-FROM utgefandi
+FROM flytjandi
+INNER JOIN lag
+ON flytjandi.lag_ID = lag.ID
 INNER JOIN diskur
-ON utgefandi.diskur_ID = diskur.ID
-ORDER BY nafn
+ON lag.diskur_ID = diskur.ID
+INNER JOIN utgefandi
+ON diskur.utgefandi_ID = utgefandi.ID
+ORDER BY utgefandi.nafn
 LIMIT 2
 
 g.
 SELECT flytjandi.nafn, utgefandi.nafn
-FROM lag
-INNER JOIN
+FROM flytjandi
+INNER JOIN lag
+ON flytjandi.lag_ID = lag.ID
+INNER JOIN diskur
+ON lag.diskur_ID = diskur.ID
+INNER JOIN utgefandi
+ON diskur.utgefandi_ID = utgefandi.ID
