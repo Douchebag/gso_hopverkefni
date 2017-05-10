b9633ecaad9b45be8d87f3c85c41c14da86e6dff
Ððµ¢hêßåg <yesimdingo@gmail.com>
2017-05-10 14:39:54 +0000

    fyrirspurnir
    allar fyrirspurnir komnar

--- flyjandi.sql
+++ flyjandi.sql
@@ -1,9 +1,9 @@
 INSERT INTO flytjandi
 (nafn, faedingardagur, Danardagur, thema_id, flokkur_ID, lag_ID)
 VALUES
-("Gorillaz", "1998-03-02", NULL, "15, 16, 18, 19, ", "6", "18, 19"),
-("Kendrick Lamar", "1987-06-17", NULL, "1, 2, 3, 4, 5, 6, 7", "3", "3, 8, 9, 12, 13, 20"),
-("Ray Davies", "1944-06-21", NULL, "", "", "1, 2"),
-("Sheryl Crow", "1962-02-11", NULL, "", "", "21"),
-("The Chainsmokers", "2012-11-29", NULL, "", "", "22"),
-("Tupac Shakur", "1971-06-16", "1996-09-13", "", "", "4, 5, 6, 7, 11, 14, 15, 16, 17")
+("Gorillaz", "1998-03-02", NULL, 19, 6, 19),
+("Kendrick Lamar", "1987-06-17", NULL, 7, 3, 20),
+("Ray Davies", "1944-06-21", NULL, 20, 4, 1),
+("Sheryl Crow", "1962-02-11", NULL, 17, 1, 21),
+("The Chainsmokers", "2012-11-29", NULL, 19, 2, 22),
+("Tupac Shakur", "1971-06-16", "1996-09-13", 6, 3, 17)
--- fyrirspurnir.sql
+++ fyrirspurnir.sql
@@ -29,7 +29,7 @@ SELECT nafn
 FROM diskur
 WHERE utgafudagur > '2010-01-01'
 
-f.*****
+f.
 SELECT diskur.nafn, flytjandi.nafn, diskur.utgafudagur
 FROM flytjandi
 INNER JOIN lag
@@ -38,7 +38,7 @@ INNER JOIN diskur
 ON lag.diskur_ID = diskur.ID
 INNER JOIN utgefandi
 ON diskur.utgefandi_ID = utgefandi.ID
-ORDER BY utgefandi.nafn
+ORDER BY 'utgefandi.nafn' ASC
 LIMIT 2
 
 g.
@@ -50,3 +50,67 @@ INNER JOIN diskur
 ON lag.diskur_ID = diskur.ID
 INNER JOIN utgefandi
 ON diskur.utgefandi_ID = utgefandi.ID
+ORDER BY 'lag.lengd_sek' DESC
+LIMIT 5
+
+h.
+SELECT tegund.nafn, COUNT(diskur.nafn)
+FROM lag
+INNER JOIN diskur
+ON lag.diskur_ID = diskur.ID
+INNER JOIN tegund
+ON lag.tegund_ID = tegund.ID
+GROUP BY diskur.nafn
+ORDER BY COUNT(diskur.nafn) DESC
+LIMIT 2
+
+i.
+SELECT utgefandi.nafn, COUNT(diskur.nafn)
+FROM diskur
+INNER JOIN utgefandi
+ON diskur.utgefandi_ID = utgefandi.ID
+GROUP BY utgefandi.nafn
+ORDER BY COUNT(diskur.nafn) ASC
+LIMIT 1
+
+j.*
+SELECT diskur.nafn, tegund.nafn, utgefandi.nafn
+FROM lag
+INNER JOIN diskur
+ON lag.diskur_ID = diskur.ID
+INNER JOIN tegund
+ON lag.tegund_ID = tegund.ID
+INNER JOIN utgefandi
+ON diskur.utgefandi_ID = utgefandi.ID
+WHERE diskur.utgafudagur BETWEEN '1998-01-01' AND '2017-01-01'
+GROUP BY utgefandi.nafn
+
+k.
+SELECT nafn, COUNT(nafn)
+FROM lag
+WHERE nafn LIKE 'A%' AND nafn LIKE '%s%'
+group by nafn
+
+l.
+SELECT nafn, TIMESTAMPDIFF(YEAR, faedingardagur, CURDATE())
+FROM flytjandi
+
+m.
+SELECT AVG(lengd_sek) as medaltalSek
+FROM lag
+
+n.
+SELECT flytjandi.nafn, COUNT(lag.nafn)
+FROM flytjandi
+INNER JOIN lag
+ON flytjandi.lag_ID = lag.ID
+GROUP BY flytjandi.nafn
+HAVING COUNT(lag.nafn) > 4
+
+o.
+SELECT flytjandi.nafn, lag.nafn
+FROM flytjandi
+INNER JOIN lag
+ON flytjandi.lag_ID = lag.id
+ORDER BY faedingardagur ASC
+LIMIT 1
